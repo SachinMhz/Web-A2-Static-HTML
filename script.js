2d1d670285257758b5e36864d7222c816aa2024c
SachinMhz <sachin.mhz.sm@gmail.com>
2024-03-05 10:46:53 -0500

    mid-term: responsive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Event listener for showing the nav menu in small screen size.
 document.querySelector(".nav-menu").addEventListener("click", () => {
   let navList = document.querySelector(".nav-list");
 
@@ -8,21 +9,30 @@ document.querySelector(".nav-menu").addEventListener("click", () => {
   }
 });
 
+// Event listener to close the nav-menu after the menu items are clicked.
+document.querySelector(".nav-list").addEventListener("click", () => {
+  let navList = document.querySelector(".nav-list");
+  navList.className = "nav-list";
+});
+
+/* 
+When the each papers are clicked, the clickde paper is shown properly in the DOM.
+*/
 let papers = document.querySelectorAll(".paper-wrapper");
 
-papers.forEach(paper => {
-    paper.addEventListener('click', () => {
-        let clickedClass = paper.classList[1];
-        
-        // Swap classes between the clicked paper and the first paper
-        papers.forEach(paperElement => {
-            if (paperElement.classList.contains('paper-1')) {
-                paperElement.classList.remove('paper-1');
-                paperElement.classList.add(clickedClass);
-            } else if (paperElement.classList.contains(clickedClass)) {
-                paperElement.classList.remove(clickedClass);
-                paperElement.classList.add('paper-1');
-            }
-        });
+papers.forEach((paper) => {
+  paper.addEventListener("click", () => {
+    let clickedClass = paper.classList[1];
+
+    // Swap classes between the clicked paper and the first paper
+    papers.forEach((paperElement) => {
+      if (paperElement.classList.contains("paper-1")) {
+        paperElement.classList.remove("paper-1");
+        paperElement.classList.add(clickedClass);
+      } else if (paperElement.classList.contains(clickedClass)) {
+        paperElement.classList.remove(clickedClass);
+        paperElement.classList.add("paper-1");
+      }
     });
+  });
 });
